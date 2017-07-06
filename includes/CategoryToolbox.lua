f0d327a5c4fc82f283af2db664a3f74c83bb3cb2
Valerio Bozzolan <bozzolan.valerio@educ.di.unito.it>
2017-07-06 03:36:10 +0200

    -hello() +categoryHasPage()

--- includes/CategoryToolbox.lua
+++ includes/CategoryToolbox.lua
@@ -20,9 +20,47 @@ function cattools.setupInterface( options )
 	package.loaded['mw.ext.cattools'] = cattools
 end
 
--- ask
-function cattools.hello( parameters )
-	return php.hello( parameters )
+local function has_prefix(title, prefix)
+	return nil ~= mw.ustring.find(title, prefix .. ':')
+end
+
+local function strip_prefix(title, prefix)
+	local len = mw.ustring.len(prefix .. ':')
+	return mw.ustring.sub(title, len)
+end
+
+--[[
+* @param string title Title in various form e.g. "Category:Foo", "Categoria:Foo", or only "Foo"
+* @param int namespace Namespace number
+* @return string Title without namespace prefix e.g. "Foo" without "Category:"
+]]--
+local function only_name(title, namespace)
+	local ns = mw.site.namespaces[namespace]
+
+	local ns_name, ns_canonical = ns.name, ns.canonicalName
+
+	if has_prefix(title, ns_name) then
+		return strip_prefix(title, ns_name)
+	end
+
+	if has_prefix(title, ns_canonical) then
+		return strip_prefix(title, ns_canonical)
+	end
+
+	return title
+end
+
+function cattools.categoryHasPage( category, page_namespace, page_title )
+	category = only_name(category, 14)
+	return php.categoryHasPage( category, page_namespace, page_title )
+end
+
+function cattools.categoryHasTitleObject( category, title )
+	title = title or mw.title.getCurrentTitle()
+	if not title.namespace or not title.text then
+		error("Not a valid title object")
+	end
+	return php.categoryHasPage( category, title.namespace, title.text )
 end
 
 return cattools
--- includes/CategoryToolbox_LuaLibrary.php
+++ includes/CategoryToolbox_LuaLibrary.php
@@ -17,38 +17,132 @@
 */
 
 class CategoryToolbox_LuaLibrary extends Scribunto_LuaLibraryBase {
+
+	protected $db;
+
 	function register() {
-		$entry_points = [
-			'hello' => [ $this, 'hello'],
-		];
 
-		$this->getEngine()->registerInterface(
-			__DIR__ . '/CategoryToolbox.lua',
-			$entry_points,
-			[]
-		);
-	}
+		$this->getEngine()->registerInterface( __DIR__ . '/CategoryToolbox.lua', [
+			'categoryHasPage' => [ $this, 'categoryHasPage' ],
+			'categoryPages'   => [ $this, 'categoryPages' ]
+		] );
 
-	public function hello() {
-		return [ $this->convertArrayToLuaTable( [1, 2, 3, 4, 5, 'banana'] ) ];
+		// DB_REPLICA is not defined in MediaWiki 1.27.3
+		$this->db = wfGetDB( defined('DB_REPLICA') ? DB_REPLICA : DB_MASTER );
 	}
 
 	/**
-	 * This takes an array and converts it so, that the result is a viable lua table.
+	 * To retrieve all the pages in a certain category.
+	 *
+	 * @param string $category_name Category name (without prefix)
+	 * @param int $page_namespace Page namespace (number)
+	 * @return mixed Lua object
+	 */
+	public function categoryPages($category_name, $page_namespace = null, $limit = 50, $offset = null) {
+		return self::categoryLinksToLuaTable( $this->selectCategoryLinks($category_name, $page_namespace, null, $limit, $offset) );
+	}
+
+	/**
+	 * To retrieve if a certain page is linked into a category.
+	 *
+	 * @param string $category_name Category name (without prefix)
+	 * @param string $page_title Page title (without prefix)
+	 * @param int $page_namespace Page namespace (number)
+	 */
+	public function categoryHasPage($category_name, $page_namespace, $page_title) {
+		return self::toLuaBool( 1 === $this->selectCategoryLinks($category_name, $page_namespace, $page_title)->numRows() );
+	}
+
+	/**
+	 * To retrieve what is linked into a category.
+	 *
+	 * @param string $category_name Category name (without prefix)
+	 * @param int $page_namespace Restrict to a specific namespace (number)
+	 * @param string $page_title Restrict to a specific page title (without prefix)
+	 * @param int $limit Result limit. Even if this method is intended only to retrieve a couple of sub-categories, it should be used also for pages.
+	 * @return IResultWrapper|bool
+	 */
+	private function selectCategoryLinks($category_name, $page_namespace = null, $page_title = null, $limit = 50, $offset = null) {
+
+		$fields = [
+			'cl_type',
+			'page_id',
+			'page_title',
+			'page_namespace'
+		];
+
+		$conditions = [
+			'cl_to' => $category_name,
+			'cl_from = page_id'
+		];
+
+		if( null !== $page_namespace ) {
+			$conditions['page_namespace'] = $page_namespace;
+		}
+
+		if( null === $page_title ) {
+			// Too generic means more results
+			$this->incrementExpensiveFunctionCount();
+		} else {
+			$conditions['page_title'] = $page_title;
+		}
+
+		$options = [
+			'LIMIT' => $limit
+		];
+
+		if( null !== $offset ) {
+			$options['OFFSET'] = $offset;
+		}
+
+		return $this->db->select( ['categorylinks', 'page'] , $fields, $conditions, __METHOD__, $options );
+	}
+
+	/**
+	 * Giving a database result from `::selectCategoryLinks()` it returns a flat array.
+	 *
+	 * @param IResultWrapper|bool $results Result from the Wikimedia\Rdbms\Database::select
+	 * @return array Lua object
+	 */
+	private static function categoryLinksToLuaTable($results) {
+		$rows = [];
+		foreach($results as $row) {
+			$rows[] = [
+				'id'   => (int)$row->page_id,
+				'ns'   => (int)$row->page_namespace,
+				'type' =>      $row->cl_type
+			];
+		}
+		return self::toLuaTable( $rows );
+	}
+
+	/**
+	 * This takes an array and converts it so, that the result is a viable Lua table.
 	 * I.e. the resulting table has its numerical indices start with 1
-	 * If `$ar` is not an array, it is simply returned
+	 * If `$ar` is not an array, it is simply returned.
+	 *
 	 * @param mixed $ar
-	 * @return mixed array
+	 * @return mixed Lua object
 	 * @see https://github.com/SemanticMediaWiki/SemanticScribunto/blob/master/src/ScribuntoLuaLibrary.php
 	 */
-	private function convertArrayToLuaTable( $ar ) {
-		if ( is_array( $ar) ) {
+	private static function toLuaTable( $ar ) {
+		if ( is_array( $ar ) ) {
 			foreach ( $ar as $key => $value ) {
-				$ar[$key] = $this->convertArrayToLuaTable( $value );
+				$ar[$key] = self::toLuaTable( $value );
 			}
 			array_unshift( $ar, '' );
 			unset( $ar[0] );
 		}
 		return $ar;
+	}
+
+	/**
+	 * Workaround for a Scribunto_LuaLibraryBase bug with boolean values.
+	 *
+	 * @param int $is Boolean value
+	 * @return mixed|null
+	 */
+	private static function toLuaBool($is) {
+		return $is ? [ 1 ] : null;
 	}
 }
